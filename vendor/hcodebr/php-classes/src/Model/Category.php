c15151777c3b0c08751e3ae717ab5d3ec8a6417a
Rodrigo Gonzales <rodrigodeag@gmail.com>
2021-05-19 13:24:51 -0300

    Paginacao

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -84,6 +84,31 @@ class Category extends Model
 		
 	}
 
+	public function getProductsPage($page = 1, $itemsPerPage = 3)
+	{
+		$start = ($page -1) *$itemsPerPage;
+
+		$sql = new Sql();
+
+		$sql->select("
+			SELECT SQL_CALC_FOUND_ROWS * FROM tb_products a
+			INNER JOIN tb_productscategories b on a.idproduct = b.idproduct
+			INNER JOIN tb_categories c on c.idcategory = b.idcategory
+			WHERE c.idcategory = :idcategory
+			LIMIT $start, $itemsPerPage;
+		",[
+			':idcategory'=>$this->getidcategory()
+		]);
+
+		$$resultTotal = $sql->select("SELECT * FROM FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>Product::checkList($results),
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+	}
+
 	public function addProduct(Product $product)
 	{
 		$sql = new Sql();
